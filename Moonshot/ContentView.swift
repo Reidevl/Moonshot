24beee24074002a8b9714ce5085c1ba28baee044
Reiwil Lugo <reiwillugo@gmail.com>
2024-03-13 13:53:30 -0400

    How to layout views in a scrolling grid

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,36 +7,18 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
 
 struct ContentView: View {
+    let layout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+    ]
+    
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
+        ScrollView {
+            LazyVGrid(columns: layout, spacing: 20) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
                 }
-            }
-            """
-            
-            // more code to come
-            
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
             }
         }
     }
